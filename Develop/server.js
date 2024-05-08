fe61978fcf572d81b299f3c19f2df0afeadc6d12
dom <dominic.decapite@gmail.com>
2024-05-07 20:07:02 -0400

    Update routes.js

--- Develop/server.js
+++ Develop/server.js
@@ -20,6 +20,9 @@ app.get('/', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/index.html'))
   );
 
+  //GET route for notes page
+
+
   app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/notes.html'))
   );
