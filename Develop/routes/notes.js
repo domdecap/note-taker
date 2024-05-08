90e28bcb3edf0cd0d80636bb104aab665662377c
dom <dominic.decapite@gmail.com>
2024-05-07 21:57:35 -0400

    Various bug fixes

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,33 +1,49 @@
 const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const {
-    readFromFile,
-    readAndAppend,
-    // writeToFile,
-  } = require('../helpers/fsUtils');
+  readFromFile,
+  readAndAppend,
+  // writeToFile,
+} = require('../helpers/fsUtils');
 
-  notes.get('/', (req, res) => {
-    readFromFile('../db/db.json').then((data) => res.json(JSON.parse(data)));
-  });
+notes.get('/', (req, res) => {
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
 
-  notes.post('/', (req, res) => {
-    console.log(req.body);
-  
-    const { title, text } = req.body;
-  
-    if (req.body) {
-      const newNote = {
-        title,
-        text,
-        id: uuidv4(),
-      };
-  
-      readAndAppend(newNote, '../db/db.json');
+// notes.delete('/:id', (req, res) => {
+//   const noteId = req.params.tip_id;
+//   readFromFile('./db/db.json')
+//     .then((data) => JSON.parse(data))
+//     .then((json) => {
+//       // Make a new array of all tips except the one with the ID provided in the URL
+//       const result = json.filter((note) => note.id !== noteId);
 
-      res.json(`Note added`);
-    } else {
-      res.error('Error in adding note');
-    }
-  });
+//       // Save that array to the filesystem
+//       writeToFile('./db/db.json', result);
 
-  module.exports = notes;
+//       // Respond to the DELETE request
+//       res.json(`Item ${noteId} has been deleted 🗑️`);
+//     });
+// });
+
+notes.post('/', (req, res) => {
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    readAndAppend(newNote, './db/db.json');
+
+    res.json(`Note added`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
